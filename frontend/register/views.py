4005355b604b19d12175fdf1a6e2a16fe57a057b
Andreea <76445060+MazereAndreea@users.noreply.github.com>
2025-04-21 01:26:46 +0300

    Reapply "Starting django"
    This reverts commit 19897be992a3c5e9263c50fbe1162ffbc42391e5.

---
+++ frontend/register/views.py
@@ -0,0 +1,15 @@
+# register/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully!')
+            return redirect('login')  # Redirect to login page after successful registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'register/register.html', {'form': form})
