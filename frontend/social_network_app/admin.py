c22928707bed6c51487297feeeb1c05989269332
Adrian Cirstea <54354938+ShadowsAdi@users.noreply.github.com>
2025-05-19 20:51:43 +0300

    Implemented `AI Moderator` (#5)
    - integrated NLP Analysis with llama3.2:latest model
    - added necessary tables

--- frontend/social_network_app/admin.py
+++ frontend/social_network_app/admin.py
@@ -1,14 +1,32 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, Role
-
-@admin.register(Role)
-class RoleAdmin(admin.ModelAdmin):
-    pass
+from .models import CustomUser, NLPAnalysisLog, Post
 
 @admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('role',)}),
+class CustomUser(UserAdmin):
+    list_display = (
+        'username', 'email', 'first_name', 'last_name', 'is_staff', 'get_user_groups'
     )
-    list_display = UserAdmin.list_display + ('role',)
+
+    def get_user_groups(self, obj):
+        return ", ".join(group.name for group in obj.groups.all())
+    get_user_groups.short_description = 'Auth Groups'
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'created_at')
+
+@admin.register(NLPAnalysisLog)
+class AIAnalysisLog(admin.ModelAdmin):
+    list_display = ('id', 'target_type', 'related_object', 'is_toxic', 'run_time')
+    list_filter = ('target_type', 'is_toxic', 'run_time')
+    search_fields = ('post__content', 'comment__content', 'result')
+    readonly_fields = ('target_type', 'post', 'comment', 'is_toxic', 'result', 'run_time')
+
+    def related_object(self, obj):
+        if obj.target_type == 'post' and obj.post:
+            return f"Post #{obj.post.id}"
+        elif obj.target_type == 'commentariu' and obj.comment:
+            return f"Comment #{obj.comment.id}"
+        return '-'
+    related_object.short_description = 'Target'
